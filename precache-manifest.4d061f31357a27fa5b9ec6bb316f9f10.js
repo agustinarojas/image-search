d51db08026a168c985e4afa2cc35fe2ca2b35aec
Agustina Rojas <agustinarojas2214@gmail.com>
2020-10-13 16:17:56 -0300

    Updates

---
+++ precache-manifest.4d061f31357a27fa5b9ec6bb316f9f10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54bb6adf59c66252e4068c07ff03e91a",
+    "url": "/image-search/index.html"
+  },
+  {
+    "revision": "db0e1aae5acc960b4756",
+    "url": "/image-search/static/css/main.6e64da3e.chunk.css"
+  },
+  {
+    "revision": "656ebaf7c057e037ab45",
+    "url": "/image-search/static/js/2.b58a1cb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/image-search/static/js/2.b58a1cb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db0e1aae5acc960b4756",
+    "url": "/image-search/static/js/main.380d1da9.chunk.js"
+  },
+  {
+    "revision": "e624725bef5bccc16846",
+    "url": "/image-search/static/js/runtime-main.aeb9f523.js"
+  }
+]);
